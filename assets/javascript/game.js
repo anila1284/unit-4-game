d6fff13f2baec1ea6e447b6ce177961cb99d91f1
unknown <anila1284@gmail.com>
2018-06-07 23:36:31 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,87 +2,142 @@
 
 $(document).ready(function() {
 
-    var gameStarted = false;
-    var gameOver = false;
-    var computerPick;
-    var placeHolder;
+    var gameStarted = false;        
+    var totalScore = 0;
+    var randomTotal;
+    var wins = 0;
+    var loses = 0;
+    var randomNumber1;
+    var randomNumber2;
+    var randomNumber3;
+    var randomNumber4;
 
     $(document).on('keypress', function(e) {
 
-        var letters = ["Apple", "Banana", "Kitten", "Bingo", "Fish", "McDonald", "Mickey", "JackAndJill"];     
-
+        
         if(!gameStarted){
         //generate a new random number between 1 and 8.
-        var random = Math.floor(Math.random() * 8) + 1;
+       
+        randomTotal = Math.floor(Math.random()*(120-19+1)+19);
 
-        computerPick = letters[random];
+        $(".startGame").text("");
 
-        placeHolder = "";
+        randomNumber1 = Math.floor(Math.random() * 12) + 1;
+        randomNumber2 = Math.floor(Math.random() * 12) + 1;
 
-        for(var i = 0; i < computerPick.length; i ++){
+        while(randomNumber1 == randomNumber2){
+          randomNumber2 = Math.floor(Math.random() * 12) + 1;
+        }
 
-              placeHolder = placeHolder + " _ ";                       
-        }     
-     
-      $("#computer-pick").text(placeHolder);
-      gameStarted = true;
+        randomNumber3 = Math.floor(Math.random() * 12) + 1;
 
-      console.log(computerPick);
+        while(randomNumber1 == randomNumber3 || randomNumber2 == randomNumber3){
 
-      //console.log(String.fromCharCode(e.keyCode));
-      
-    }
-    else{
+          randomNumber3 = Math.floor(Math.random() * 12) + 1;
+        }
 
-        var keynum = String.fromCharCode(e.keyCode);
-        //compare
+        randomNumber4 = Math.floor(Math.random() * 12) + 1;
 
-         if(computerPick.includes(keynum)){
+        while(randomNumber1 == randomNumber4 || randomNumber2 == randomNumber4 || randomNumber3 == randomNumber4){
 
-          //alert("true: " + keynum);
-
-          var index = computerPick.indexOf(keynum);
-
-          
-          console.log(index);
-
-          var guessedWord =  placeHolder.replaceAt(index + 1, keynum); 
-
-          console.log(guessedWord);
-          console.log(placeHolder);
-
-          $("#computer-pick").text(guessedWord);
+          randomNumber4 = Math.floor(Math.random() * 12) + 1;
+        }
 
          
+        //$("#page_navigation1").attr("id","page_navigation1");
 
-          // jQuery alternative to: var newDiv = document.createElement("div");
-          var newButton = $("<button>");
+        $('#crystal1').attr("value", randomNumber1);
+
+        $('#crystal2').attr("value", randomNumber2);
+
+        $('#crystal3').attr("value", randomNumber3);
+
+        $('#crystal4').attr("value", randomNumber4);
+
+
+        console.log("randomTotal: " + randomTotal);
+             
+        console.log("randomNumber1: " + randomNumber1);
+             
+        console.log("randomNumber2: " + randomNumber2);
+             
+        console.log("randomNumber3: " + randomNumber3);
+
+        console.log("randomNumber4: " + randomNumber4);        
+       
+        gameStarted = true;        
+
+        $("#computer-pick").text(randomTotal);
       
-          
-          // jQuery alternative to: document.querySelector("#empty-div").appendChild(newDiv);
-          $(".guesses").append(newButton);
-      
-          // If we need to apply some CSS, we can quickly do so, using the jQuery ".attr" method.
-          var btnID = "button-" + keynum;
-          newButton.attr("id", btnID);
-          newButton.attr("class", "btn btn-danger btn-choice");
-
-
-          $(btnID).html("<h1>" + keynum + "</h1>");
-          
-          gameStarted = false;
-          
-         }
-         
+    }
+    else{       
      
 
+    }  
+
+    });
+
+
+    $(".crystal").click(function(e) {
+
+
+      if(gameStarted){
+        
+      totalScore = totalScore + parseInt($(this).attr("value"));
+      $("#result").text(totalScore);
+
+      console.log(totalScore);
+
+      if(totalScore == randomTotal){
+        
+        wins = wins + 1;
+        $(".wins").text("Wins: " + wins);
+        console.log("Wins: " + wins);
+        randomNumber1 = 0;
+        randomNumber2 = 0;
+        randomNumber3 = 0;
+        randomNumber4 = 0;
+        randomTotal = 0;
+        totalScore = 0;  
+        gameStarted = false;
+        $('#crystal1').attr("value", randomNumber1);
+
+        $('#crystal2').attr("value", randomNumber2);
+
+        $('#crystal3').attr("value", randomNumber3);
+
+        $('#crystal4').attr("value", randomNumber4);
+        $("#result").text("");
+        $("#computer-pick").text("");
+
+      }
+      else if(totalScore > randomTotal){       
+
+        loses = loses + 1;          
+        console.log("Loses: " + loses);         
+        $(".losses").text("Loses: " + loses);
+        $(".startGame").text("You Lost. Press any key to start the game again");
+        $('.startGame').attr("style", "color: Red");    
+        randomNumber1 = 0;
+        randomNumber2 = 0;
+        randomNumber3 = 0;
+        randomNumber4 = 0;
+        randomTotal = 0;
+        totalScore = 0; 
+        gameStarted = false;
+        $('#crystal1').attr("value", randomNumber1);
+
+        $('#crystal2').attr("value", randomNumber2);
+
+        $('#crystal3').attr("value", randomNumber3);
+
+        $('#crystal4').attr("value", randomNumber4);
+        $("#result").text("");
+        $("#computer-pick").text("");
+
+      }     
     }
-    String.prototype.replaceAt=function(index, replacement) {
-      return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
-
-    };
-
-
+        
     });
 
   });
